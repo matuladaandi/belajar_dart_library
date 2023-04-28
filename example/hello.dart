0517297de535e098c5bb517a8aeb00ad6881fadd
andi <echo.matuladaandi@gmail.com>
2023-04-28 08:41:01 +0800

    belajar dart package library

---
+++ example/hello.dart
@@ -0,0 +1,13 @@
+import 'package:belajar_library/lib_export.dart';
+import 'package:belajar_library/src/customer.dart';
+
+void main() {
+  print(sayHello('amdi'));
+
+  Customer customer = Customer();
+  var category = Category();
+  var produck = Product();
+
+  print(produck.toString());
+
+}
